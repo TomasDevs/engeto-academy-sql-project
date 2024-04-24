c4e41a6654a5483f6ee9399e2e735c3c01924215
Tomáš Štveráček <tomas.stveracek@gmail.com>
2024-04-24 18:40:46 +0200

    Create aggregated primary table for wage and price data

--- t_tomas_stveracek_project_SQL_primary_final.sql
+++ t_tomas_stveracek_project_SQL_primary_final.sql
@@ -1,34 +1,48 @@
 DROP TABLE IF EXISTS t_tomas_stveracek_project_SQL_primary_final;
 
 CREATE TABLE t_tomas_stveracek_project_SQL_primary_final AS
+WITH payroll_aggregate AS (
+	SELECT
+		payroll_year,
+		payroll_quarter,
+		industry_branch_code,
+		AVG(value) AS avg_wage
+	FROM
+		czechia_payroll cp 
+	GROUP BY
+		payroll_year,
+		payroll_quarter,
+		industry_branch_code
+),
+price_aggregate AS (
+	SELECT 
+		YEAR(date_from) AS year,
+		QUARTER(date_from) AS quarter, 
+		category_code,
+		AVG(value) AS avg_price
+	FROM 
+		czechia_price cp 
+	GROUP BY
+		YEAR(date_from),
+		QUARTER(date_from), 
+        category_code
+)
 SELECT
-	cp.payroll_year AS year,
-	cp.payroll_quarter AS quarter,
-	cpib.name AS industry_branch,
-	cpvt.name AS value_type,
-	cpu.name AS unit,
-	cpc.name AS category,
-	AVG(cpr.value) AS average_price,
-	AVG(cp.value) AS average_wage
+    pa.payroll_year AS year,
+    pa.payroll_quarter AS quarter,
+    cpib.name AS industry_branch,
+    pa.avg_wage AS average_wage,
+    cpc.name AS category,
+    pra.avg_price AS average_price
 FROM
-	czechia_payroll cp 
+	payroll_aggregate pa
 JOIN
-	czechia_payroll_industry_branch cpib ON cp.industry_branch_code = cpib.code
-JOIN 
-	czechia_payroll_value_type cpvt ON cp.value_type_code = cpvt.code
+	price_aggregate pra ON pa.payroll_year = pra.year AND pa.payroll_quarter= pra.quarter
 JOIN
-	czechia_payroll_unit cpu ON cp.unit_code = cpu.code
+	czechia_payroll_industry_branch cpib ON pa.industry_branch_code = cpib.code
 JOIN
-	czechia_price cpr ON YEAR(cpr.date_from) = cp.payroll_year AND QUARTER(cpr.date_from) = cp.payroll_quarter
-JOIN
-	czechia_price_category cpc ON cpr.category_code = cpc.code
-WHERE
-	cp.payroll_year = YEAR(cpr.date_from) AND
-	cp.payroll_quarter = QUARTER(cpr.date_from)
-GROUP BY
-	cp.payroll_year,
-	cp.payroll_quarter,
-	cpib.name,
-	cpvt.name,
-	cpu.name,
-	cpc.name;
+	czechia_price_category cpc ON pra.category_code = cpc.code;
+
+
+
+
