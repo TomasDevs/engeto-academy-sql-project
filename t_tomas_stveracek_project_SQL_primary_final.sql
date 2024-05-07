64d44292ed9ebe8b45f5b7f43abce02ffd08c190
Tomáš Štveráček <tomas.stveracek@gmail.com>
2024-05-07 12:40:55 +0200

    Formatting sql script

--- t_tomas_stveracek_project_SQL_primary_final.sql
+++ t_tomas_stveracek_project_SQL_primary_final.sql
@@ -2,29 +2,29 @@ DROP TABLE IF EXISTS t_tomas_stveracek_project_SQL_primary_final;
 
 CREATE TABLE t_tomas_stveracek_project_SQL_primary_final AS
 WITH payroll_aggregate AS (
-	SELECT
-		payroll_year,
-		payroll_quarter,
-		industry_branch_code,
-		AVG(value) AS avg_wage
-	FROM
-		czechia_payroll cp 
-	GROUP BY
-		payroll_year,
-		payroll_quarter,
-		industry_branch_code
+    SELECT
+        payroll_year,
+        payroll_quarter,
+        industry_branch_code,
+        AVG(value) AS avg_wage
+    FROM
+        czechia_payroll
+    GROUP BY
+        payroll_year,
+        payroll_quarter,
+        industry_branch_code
 ),
 price_aggregate AS (
-	SELECT 
-		YEAR(date_from) AS year,
-		QUARTER(date_from) AS quarter, 
-		category_code,
-		AVG(value) AS avg_price
-	FROM 
-		czechia_price cp 
-	GROUP BY
-		YEAR(date_from),
-		QUARTER(date_from), 
+    SELECT 
+        YEAR(date_from) AS year,
+        QUARTER(date_from) AS quarter, 
+        category_code,
+        AVG(value) AS avg_price
+    FROM 
+        czechia_price
+    GROUP BY
+        YEAR(date_from),
+        QUARTER(date_from), 
         category_code
 )
 SELECT
@@ -35,14 +35,10 @@ SELECT
     cpc.name AS category,
     pra.avg_price AS average_price
 FROM
-	payroll_aggregate pa
+    payroll_aggregate pa
 JOIN
-	price_aggregate pra ON pa.payroll_year = pra.year AND pa.payroll_quarter= pra.quarter
+    price_aggregate pra ON pa.payroll_year = pra.year AND pa.payroll_quarter = pra.quarter
 JOIN
-	czechia_payroll_industry_branch cpib ON pa.industry_branch_code = cpib.code
+    czechia_payroll_industry_branch cpib ON pa.industry_branch_code = cpib.code
 JOIN
-	czechia_price_category cpc ON pra.category_code = cpc.code;
-
-
-
-
+    czechia_price_category cpc ON pra.category_code = cpc.code;
