debbd2ff58b5cff774a7dbbce2165856788e3369
Tomáš Štveráček <tomas.stveracek@gmail.com>
2024-05-07 12:49:29 +0200

    Refine query for calculating purchasable units of milk and bread

--- question2_number_purchases_milk_bread.sql
+++ question2_number_purchases_milk_bread.sql
@@ -3,13 +3,26 @@
  * Kolik je možné si koupit litrů mléka a kilogramů chleba za první a poslední srovnatelné období v dostupných datech cen a mezd?
  */
 
+-- Dotaz vypočítává, kolik jednotek mléka nebo chleba lze koupit za průměrnou mzdu
 SELECT
-  year,
-  SUM(CASE WHEN category LIKE '%mléko%' THEN average_wage / average_price ELSE 0 END) AS liters_of_milk,
-  SUM(CASE WHEN category LIKE '%chléb%' THEN average_wage / average_price ELSE 0 END) AS kg_of_bread
+	year,
+	category,
+	ROUND(AVG(average_price), 2) AS avg_price,
+	ROUND(AVG(average_wage), 2) AS avg_wage,
+	ROUND(AVG(average_wage) / AVG(average_price)) AS units_purchasable
 FROM
-  t_tomas_stveracek_project_SQL_primary_final
+	t_tomas_stveracek_project_SQL_primary_final
 WHERE
-  year IN (2006, 2018)
+	year IN (2006, 2018) AND
+	(category LIKE '%mléko%' OR category LIKE '%chléb%')
 GROUP BY
-  year;
+	year, category
+ORDER BY
+	year, category;
+
+/*
+ * Odpověď:
+ * Analýza ukazuje zlepšní kupní síly chleba a mléka mezi roky 2006 a 2018.
+ * Zatímco v roce 2006 bylo možné koupit za průměrnou mzdu 1.217 kg chleba a 1.363 litrů mléka,
+ * v roce 2018 se toto množství zvýšilo na 1.267 kg chleba a 1.55 litrů mléka.
+ */ 
